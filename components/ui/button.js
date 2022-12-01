d6e05c227287114cb8ea6eaa419f9fc708f0cb16
raphaelaquino23 <raphaelaquino23@gmail.com>
2022-12-01 14:12:45 +0800

    add search component

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,17 +1,21 @@
 import Link from "next/link";
 import classes from "../../styles/button.module.css";
 
-function Button({ children, link }) {
-  return (
-    <Link
-      href={{
-        pathname: link,
-      }}
-      className={classes.btn}
-    >
-      {children}
-    </Link>
-  );
+function Button({ children, link, onClick }) {
+  if (link) {
+    return (
+      <Link
+        href={{
+          pathname: link,
+        }}
+        className={classes.btn}
+      >
+        {children}
+      </Link>
+    );
+  }
+
+  return <button className={classes.btn} onClick={onClick}>{children}</button>
 }
 
 export default Button;
