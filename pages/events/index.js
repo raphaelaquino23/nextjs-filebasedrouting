c61e869401f0d103152feb2980fb4a265b674547
raphaelaquino23 <raphaelaquino23@gmail.com>
2022-12-01 14:20:10 +0800

    add eventssearch logic

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,23 @@
 import { getAllEvents } from '../../dummy-data'
+import { useRouter } from 'next/router';
+
 import EventList from '../../components/events/event-list';
 import EventsSearch from '../../components/events/events-search';
 
 function EventsPage() {
   const events = getAllEvents()
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    router.push({
+      pathname: '/events/[year]/[month]',
+      query: {year: year, month: month}
+    })
+  }
 
   return (
     <div>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler}/>
       <EventList items={events} />
     </div>
   );
